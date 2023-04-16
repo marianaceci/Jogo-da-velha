bb416d968ee9647c535a4bdd1ae233c3de199623
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-04-16 15:58:02 -0300

    game ready

---
+++ script.js
@@ -0,0 +1,101 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessage = document.querySelector("[data-winning-message]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const restart = document.querySelector("[data-restart]");
+
+let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const startGame = () => {
+  isCircleTurn = false;
+
+  for (const cell of cellElements) {
+    cell.classList.remove("o");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  setBoardHoverClass();
+  winningMessage.classList.remove("show-winning-message");
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "O Venceu!"
+      : "X Venceu!";
+  }
+
+  winningMessage.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("x") || cell.classList.contains("o");
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const setBoardHoverClass = () => {
+  board.classList.remove("o");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("o");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+  setBoardHoverClass();
+};
+
+const handleClick = (e) => {
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "o" : "x";
+
+  placeMark(cell, classToAdd);
+
+  const isDraw = checkForDraw();
+  const isWin = checkForWin(classToAdd);
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
+    swapTurns();
+  }
+};
+
+startGame();
+restart.addEventListener("click", startGame);
